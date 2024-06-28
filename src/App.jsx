a5c150e8ed854928feb2d8fed1a3e28632187ffc
rtdongullo <dongromert@gmail.com>
2024-06-28 08:52:48 +0000

    React Components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+//import Welcome from "/pages/Welcome";
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Recipes from "./pages/Recipes"
+import Welcome from "./pages/Welcome"
+import Categories from "./pages/Categories";
+
+const router = createBrowserRouter([
+  {path:"/", element: <Welcome />},
+  {path:"/recipes", element: <Recipes />},
+  {path:"/categories", element: <Categories />}
+]);
+
+function App() {
+  return (
+    <>
+    <RouterProvider router ={router} />
+    
+    </>
+  )
+}
+
+export default App
